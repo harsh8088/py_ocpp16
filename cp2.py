6bfa4a91e0e8c5e79f3fc502a9d00aff57cad484
ev-harsh <52234750+ev-harsh@users.noreply.github.com>
2021-03-16 13:11:51 +0530

    initial commit

---
+++ cp2.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from ocpp.routing import on
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16.enums import Action, RegistrationStatus
+from ocpp.v16 import call_result
+
+
+class MyChargePoint(cp):
+    @on(Action.BootNotification)
+    def on_boot_notification(self, charge_point_vendor, charge_point_model, **kwargs):
+        return call_result.BootNotificationPayload(
+            current_time=datetime.utcnow().isoformat(),
+            interval=10,
+            status=RegistrationStatus.accepted
+        )
+
+
+async def on_connect(websocket, path):
+    """ For every new charge point that connects, create a ChargePoint instance
+    and start listening for messages.
+
+    """
+    charge_point_id = path.strip('/')
+    cp = MyChargePoint(charge_point_id, websocket)
+
+    await cp.start()
